1f23325c5aefbc87225fa0fb78f963c673bb3678
Filipe Andrade <filipeandrade.dev@gmail.com>
2020-05-11 09:52:12 -0300

    Updates mock function

--- dynamodb-antifraud/utils/data.js
+++ dynamodb-antifraud/utils/data.js
@@ -11,9 +11,9 @@ function getRandomItem() {
     const totalAmount = walletAmount + cardAmount
     const paymentStatus = faker.random.arrayElement(['AUTHORIZED', 'NOT_AUTHORIZED'])
     const paymentId = faker.random.uuid()
-    const date = moment(faker.date.between('2020-04-08', '2020-05-08'));
+    const date = moment(faker.date.between('2020-05-08T14:00:00', '2020-05-08T15:00:00'));
     const epochTime = date.unix()
-    const yearMonthDay = `${date.year()}|${date.month()}|${date.dayOfYear()}|${date.hour()}|${date.minute()}|${date.second()}`
+    const yearMonthDay = `${date.year()}|${date.month()}|${date.format('DD')}|${date.hour()}|${date.minute()}|${date.second()}`
 
     return {
         "customer_reference": "5665cb89-3392-4dd9-b1d1-9e6eb0fbd34b",
@@ -24,6 +24,7 @@ function getRandomItem() {
         "card_id": "f669a783-8c2f-4ff0-bdc9-ea343283d8cd",
         "card_id_merchant_reference": "f669a783-8c2f-4ff0-bdc9-ea343283d8cd|740f66b7-862a-4664-a704-704fcc275fa9",
         "band": "VISA",
+        "date": date.format("YYYY-MM-DDTHH:mm:ss"),
         "payment_status": paymentStatus,
         "card_ammount": cardAmount,
         "wallet_ammount": walletAmount,
